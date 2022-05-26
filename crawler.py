1e1014a19bec05099691a7cbe2ea0bb83d9d6438
PHBonifacio <pedrobonifa@gmail.com>
2022-05-26 09:31:16 -0300

    Implements functions to get infos

--- crawler.py
+++ crawler.py
@@ -1,6 +1,8 @@
+from os import link
 import requests
 from bs4 import BeautifulSoup
 
+DOMAIN = 'https://django-anuncios.solyd.com.br'
 URL = "https://django-anuncios.solyd.com.br/automoveis/"
 
 def GetURL(url):
@@ -19,5 +21,37 @@ def ParseHTML(html):
     except Exception as error:
         print(error)
 
+def GetLinks(soup):
+    if soup:
+        div = soup.find('div', class_='ui three doubling link cards')
+        cards = div.find_all('a', class_='card')
+        links = []
+        for card in cards:
+            links.append(card['href'])
+        
+        return links
 
-print(ParseHTML(GetURL(URL)))
+def GetAdName(soap):
+    if soap:
+        div = soap.find('div', class_='sixteen wide column')
+        h1 = div.find('h1', class_='ui dividing header')
+        return h1.text.replace('\n', '')
+
+def GetAdDescription(soap):
+    if soap:
+        divs = soap.find_all('div', class_='sixteen wide column')
+        for div in divs:
+            if div.find('h3'):
+                p = div.find('p')
+                return p.text.replace('\n', '')
+        
+url = GetURL(URL)
+soup = ParseHTML(url)
+links = GetLinks(soup)
+
+for link in links:
+    url = GetURL(DOMAIN + link)
+    soap = ParseHTML(url)
+    ad_name = GetAdName(soap)
+    ad_desc = GetAdDescription(soap)
+    print("{}\n\t{}\n---------------------------------------------".format(ad_name, ad_desc))
